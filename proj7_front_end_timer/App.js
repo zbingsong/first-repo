d9f9138e896f9132f794fa86d7fc323dbfe007f0
Bingsong Zeng <bingsong@usc.edu>
2022-09-13 10:45:58 -0700

    created components and empty methods needed

--- proj7_front_end_timer/App.js
+++ proj7_front_end_timer/App.js
@@ -1,21 +1,81 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Timer = props => (
+	<div>
+		
+	</div>
+)
+
+const WorkTime = props => (
+	<div>
+		<Text>Work: </Text>
+		<input type="text"></input>
+	</div>
+)
+
+const BreakTime = props => (
+	<div>
+		<Text>Break: </Text>
+		<input type="text"></input>
+	</div>
+)
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			work_time: 0,
+			break_time: 0,
+			curr_time: {
+				minute: 0,
+				second: 0,
+			},
+		}
+	}
+
+	setTime() {
+
+	}
+
+	startCountDown() {
+
+	}
+
+	pauseCountDown() {
+
+	}
+
+	resetCountDown() {
+
+	}
+
+	ring() {
+
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Timer />
+				<WorkTime />
+				<BreakTime />
+				<div>
+					<button onClick>Set</button>
+					<button>Start</button>
+					<button>Pause</button>
+					<button>Reset</button>
+				</div>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
