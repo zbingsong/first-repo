8edf5e0d4debf5f146089a0485b70b31c0b249b7
Bingsong Zeng <bingsong@usc.edu>
2022-09-13 16:02:24 -0700

    implemented setTime function, not tested

--- proj7_front_end_timer/App.js
+++ proj7_front_end_timer/App.js
@@ -1,53 +1,141 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { vibrate } from './utils';
 
 const Timer = props => (
 	<div>
-		
+
 	</div>
 )
 
-const WorkTime = props => (
-	<div>
-		<Text>Work: </Text>
-		<input type="text"></input>
-	</div>
-)
+class TimeField extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			time: {
+				hour: 0,
+				minute: 0,
+				second: 0,
+			},
+			color: {
+				hour: "black",
+				minute: "black",
+				second: "black"
+			},
+		}
+		this.handleInput = this.handleInput.bind(this)
+	}
 
-const BreakTime = props => (
-	<div>
-		<Text>Break: </Text>
-		<input type="text"></input>
-	</div>
-)
+	checkInt(input) {
+		return parseInt(input).toString() === input
+	}
+
+	handleInput(event, category) {
+		let input = event.target.value
+		if (this.checkInt(input)) {
+			input = parseInt(input)
+			if (input >= 0) {
+				if (category === "hour" || input < 60) {
+					this.setState({
+						...this.state,
+						time: {
+							...this.state.time,
+							[category]: input,
+						},
+						color: {
+							...this.state.color,
+							[category]: "black",
+						},
+					})
+					this.props.onTimeChange(this.state.time)
+				}
+			}
+		}
+		this.setState({
+			...this.state,
+			color: {
+				...this.state.color,
+				[category]: "red",
+			},
+		})
+		this.props.onTimeChange("")
+	}
+
+	render() {
+		return (
+			<div>
+				<input type="text" onChange={event => this.handleHourInput(event, "hour")} style={{color: this.state.color.hour}} /> : 
+				<input type="text" onChange={event => this.handleHourInput(event, "minute")} style={{color: this.state.color.hour}} /> :
+				<input type="text" onChange={event => this.handleHourInput(event, "second")} style={{color: this.state.color.hour}} />
+			</div>
+		)
+	}
+}
+
 
 export default class App extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			work_time: 0,
-			break_time: 0,
-			curr_time: {
+			work_time: {
+				hour: 0,
 				minute: 0,
 				second: 0,
 			},
+			break_time: {
+				hour: 0,
+				minute: 0,
+				second: 0,
+			},
+			curr_time: 0,
+			if_legal_time: true,
+			if_counting: false,
 		}
 	}
 
-	setTime() {
+	setTime(time, type) {
+		if (typeof time !== "string") {
+			this.state = {
+				...this.state,
+				[type]: time,
+				curr_time: time.hour * 3600 + time.minute * 60 + time.second,
+				if_legal_time: true,
+			}
+		} else {
+			this.state = {
+				...this.state,
+				if_legal_time: false
+			}
+		}
+	}
 
+	setWorkTime(time) {
+		this.setTime(time, "work_time")
+	}
+
+	setBreakTime(time) {
+		this.setTime(time, "break_time")
 	}
 
 	startCountDown() {
-
+		this.setState({
+			...this.state,
+			if_counting: true
+		})
 	}
 
 	pauseCountDown() {
-
+		this.setState({
+			...this.state,
+			if_counting: false
+		})
 	}
 
 	resetCountDown() {
-
+		this.setState({
+			...this.state,
+			if_counting: false
+		})
 	}
 
 	ring() {
@@ -57,14 +145,27 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Timer />
-				<WorkTime />
-				<BreakTime />
 				<div>
-					<button onClick>Set</button>
-					<button>Start</button>
-					<button>Pause</button>
-					<button>Reset</button>
+					<Timer />
+				</div>
+				<div>
+					<Text>Work:</Text>
+					<TimeField onTimeChange={this.setWorkTime} />
+				</div>
+				<div>
+					<Text>Break: </Text>
+					<TimeField onTimeChange={this.setBreakTime} />
+				</div>
+				<div>
+					<button disabled={!this.state.if_legal_time && this.state.if_counting} onClick={() => this.startCountDown()}>
+						Start
+					</button>
+					<button disabled={!this.state.if_legal_time && !this.state.if_counting} onClick={() => this.pauseCountDown()}>
+						Pause
+					</button>
+					<button disabled={!this.state.if_legal_time} onClick={() => this.resetCountDown()}>
+						Reset
+					</button>
 				</div>
 			</View>
 		)
