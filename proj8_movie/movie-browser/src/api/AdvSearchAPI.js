aec0133aabdd1e5fdaf27a3d90dace8c6e97f2fc
Bingsong Zeng <bingsong@usc.edu>
2022-11-26 16:39:54 -0800

    API for searching with more params

---
+++ proj8_movie/movie-browser/src/api/AdvSearchAPI.js
@@ -0,0 +1,20 @@
+import API_KEY from './APIKey.js';
+
+
+export default async function searchForMoviesAdvAsync(title, page) {
+    const response = await fetch(`https://www.omdbapi.com/?s=${title}&page=${page}&apikey=${API_KEY}&r=json`);
+
+    if (!response.ok) {
+        alert('Failed to fetch result list.');
+        return { movies: [], ifMoreAvailable: true };
+    }
+
+    const data = await response.json();
+    if (data.Response === 'False') {
+        // do something when no movie is found
+        return { movies: [], ifMoreAvailable: true };
+    }
+
+    const ifMoreAvailable = (page *  10 <= parseInt(data.totalResults));
+    return { movies: data.Search, ifMoreAvailable: ifMoreAvailable };
+}
