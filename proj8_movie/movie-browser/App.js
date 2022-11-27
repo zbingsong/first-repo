f4599e4026ea263ed2d7e0c3b88b649a3fa55b7d
Bingsong Zeng <bingsong@usc.edu>
2022-11-26 18:38:27 -0800

    added API calls to get configs and pass to screens

--- proj8_movie/movie-browser/App.js
+++ proj8_movie/movie-browser/App.js
@@ -1,4 +1,5 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -7,21 +8,67 @@ import ResultScreen from './src/components/result/ResultScreen';
 import DetailScreen from './src/components/detail/DetailScreen';
 import AdvSearchScreen from './src/components/search/AdvSearchScreen';
 
+import getGenreList from './src/api/GetGenreListAPI';
+import getConfig from './src/api/GetConfigAPI';
+
 
 const STACK = createNativeStackNavigator();
 
-export default function App() {
-    return (
-        <NavigationContainer style={styles.container}>
-            <STACK.Navigator initialRouteName='Search'>
-                <STACK.Screen name='Search' component={SearchScreen} />
-                <STACK.Screen name='AdvSearch' component={AdvSearchScreen} />
-                <STACK.Screen name='Result' component={ResultScreen} />
-                <STACK.Screen name='Detail' component={DetailScreen} />
-            </STACK.Navigator>
-        </NavigationContainer>
-    );
+export default class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            genres: [],
+            base_url: '',
+            poster_size: 'original',
+            ifAppReady: false
+        }
+    }
+
+    getAPIBaseInfo = async () => {
+        const genres = await getGenreList();
+        const APIconfig = await getConfig();
+        this.setState({
+            genres: genres,
+            base_url: APIconfig.base_url,
+            poster_size: APIconfig.poster_size
+        });
+    }
+
+    componentDidMount() {
+        this.getAPIBaseInfo();
+        this.setState({ifAppReady: true});
+    }
+
+    render() {
+        if (this.state.ifAppReady) {
+            const initialParams =  {
+                genres: this.state.genres, 
+                base_url: this.state.base_url, 
+                poster_size: this.state.poster_size,
+            };
+
+            return (
+                <NavigationContainer style={styles.container}>
+                    <STACK.Navigator initialRouteName='Search'>
+                        <STACK.Screen name='Search' component={SearchScreen} initialParams={initialParams} />
+                        <STACK.Screen name='AdvSearch' component={AdvSearchScreen} initialParams={initialParams} />
+                        <STACK.Screen name='Result' component={ResultScreen} initialParams={initialParams} />
+                        <STACK.Screen name='Detail' component={DetailScreen} initialParams={initialParams} />
+                    </STACK.Navigator>
+                </NavigationContainer>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Text>Loading...</Text>
+                </View>
+            );
+        }
+    }
 }
+    
 
 
 const styles = StyleSheet.create({
