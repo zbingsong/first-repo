ddc1e011950a996ad3988ad3032737482a7a8be3
Bingsong Zeng <bingsong@usc.edu>
2022-11-27 11:56:03 -0800

    creating loading and error screens

---
+++ proj8_movie/movie-browser/src/components/common/LoadingScreen.js
@@ -0,0 +1,30 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import PropTypes from 'prop-types';
+
+
+export default function LoadingScreen(props) {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: props.loadingImgAsset }} style={styles.image} />
+            <Text>Loading...</Text>
+        </View>
+    );
+}
+
+
+LoadingScreen.propTypes = {
+    loadingImgAsset: PropTypes.string,
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    image: {
+        width: 300,
+        height: 300,
+    },
+});
