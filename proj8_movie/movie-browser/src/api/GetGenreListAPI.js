5c485462be02081a78cb91de245e82d534b915c9
Bingsong Zeng <bingsong@usc.edu>
2022-11-26 16:39:33 -0800

    API for getting a map of genre ids to their names

---
+++ proj8_movie/movie-browser/src/api/GetGenreListAPI.js
@@ -0,0 +1,19 @@
+import API_KEY from './APIKey.js';
+
+
+export default async function getGenreList() {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+
+    const genres = {};
+
+    if (response.ok) {
+        const data = await response.json();
+        data.genres.forEach(genre => {
+            genres[genre.id] = genre.name
+        });
+    } else {
+        alert('Failed to fetch genre list.');
+    }
+
+    return genres;
+}
