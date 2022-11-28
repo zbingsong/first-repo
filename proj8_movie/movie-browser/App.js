295aafb1d5729fddb3e5856de1b2ff5f1627980d
Bingsong Zeng <bingsong@usc.edu>
2022-11-27 17:34:45 -0800

    added error screen for empty results or failing to load info

--- proj8_movie/movie-browser/App.js
+++ proj8_movie/movie-browser/App.js
@@ -4,19 +4,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 import SearchScreen from './src/components/search/SearchScreen';
 import ResultScreen from './src/components/result/ResultScreen';
 import DetailScreen from './src/components/detail/DetailScreen';
 import AdvSearchScreen from './src/components/search/AdvSearchScreen';
 
+import LoadingScreen from './src/components/common/LoadingScreen';
+import ErrorScreen from './src/components/common/ErrorScreen';
+
 import getGenreList from './src/api/GetGenreListAPI';
 import getConfig from './src/api/GetConfigAPI';
 
 
+SplashScreen.preventAutoHideAsync();
+
 const STACK = createNativeStackNavigator();
 const FONT_PATH = './assets/fonts/AmazonEmber_Bd.ttf';
 const DEFAULT_IMAGE_PATH = './assets/img/image-available-icon-flat-vector.jpg';
+const LOADING_IMAGE_PATH = './assets/img/loading-loading-gif.gif';
+const ERROR_IMAGE_PATH = './assets/img/error-image-generic.png';
 
 export default class App extends React.Component {
 
@@ -27,6 +35,8 @@ export default class App extends React.Component {
             baseUrl: '',
             posterSize: 'original',
             defaultImgAsset: null,
+            loadingImgAsset: null,
+            errorImgAsset: null,
             ifAppReady: false
         }
     }
@@ -38,8 +48,16 @@ export default class App extends React.Component {
     }
 
     loadDefaultImg = async () => {
-        const [{ localUri }] = await Asset.loadAsync(require(DEFAULT_IMAGE_PATH));
-        this.setState({defaultImgAsset: localUri}, () => console.log(localUri));
+        const loadedAssets = await Asset.loadAsync([
+            require(DEFAULT_IMAGE_PATH), 
+            require(LOADING_IMAGE_PATH),
+            require(ERROR_IMAGE_PATH)
+        ]);
+        this.setState({
+            defaultImgAsset: loadedAssets[0].localUri, 
+            loadingImgAsset: loadedAssets[1].localUri,
+            errorImgAsset: loadedAssets[2].localUri
+        });
     }
 
     getAPIBaseInfo = async () => {
@@ -52,48 +70,60 @@ export default class App extends React.Component {
         });
     }
 
+    completeAPICalls = async () => {
+        await Promise.all([
+            this.loadFonts(), 
+            this.getAPIBaseInfo(), 
+            this.loadDefaultImg()
+        ]);
+        this.setState(
+            { ifAppReady: true }, 
+            async () => { await SplashScreen.hideAsync(); }
+        );
+    }
+
     componentDidMount() {
-        (async () => {
-            await Promise.all([this.loadFonts(), this.getAPIBaseInfo(), this.loadDefaultImg()]);
-        })();
-        this.setState({ifAppReady: true});
+        this.completeAPICalls();
     }
 
     render() {
-        if (this.state.ifAppReady) {
-            const initialParams =  {
-                genres: this.state.genres, 
-                baseUrl: this.state.baseUrl, 
-                posterSize: this.state.posterSize,
-                defaultImgAsset: this.state.defaultImgAsset,
-            };
-
-            return (
-                <NavigationContainer style={styles.container}>
-                    <STACK.Navigator initialRouteName='Search'>
-                        <STACK.Screen name='Search' component={SearchScreen} initialParams={initialParams} />
-                        <STACK.Screen name='AdvSearch' component={AdvSearchScreen} initialParams={initialParams} />
-                        <STACK.Screen name='Result' component={ResultScreen} initialParams={initialParams} />
-                        <STACK.Screen name='Detail' component={DetailScreen} initialParams={initialParams} />
-                    </STACK.Navigator>
-                </NavigationContainer>
-            );
-        } else {
-            return (
-                <View style={styles.container}>
-                    <Text>Loading...</Text>
-                </View>
-            );
+        if (!this.state.ifAppReady) {
+            return null;
         }
+
+        const initialParams =  {
+            genres: this.state.genres, 
+            baseUrl: this.state.baseUrl, 
+            posterSize: this.state.posterSize,
+            defaultImgAsset: this.state.defaultImgAsset,
+            loadingImgAsset: this.state.loadingImgAsset,
+        };
+
+        return (
+            <NavigationContainer style={styles.container}>
+                <STACK.Navigator initialRouteName='Search'>
+                    <STACK.Screen name='Search' component={SearchScreen} 
+                        initialParams={initialParams} />
+                    <STACK.Screen name='AdvSearch' component={AdvSearchScreen} 
+                        initialParams={initialParams} />
+                    <STACK.Screen name='Result' component={ResultScreen} 
+                        initialParams={initialParams} />
+                    <STACK.Screen name='Detail' component={DetailScreen} 
+                        initialParams={initialParams} />
+                    <STACK.Screen name='Loading' component={LoadingScreen} 
+                        initialParams={{ loadingImgAsset: this.state.loadingImgAsset }} />
+                    <STACK.Screen name='Error' component={ErrorScreen}
+                        initialParams={{ errorImgAsset: this.state.errorImgAsset }} />
+                </STACK.Navigator>
+            </NavigationContainer>
+        );
     }
 }
     
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
---
+++ proj8_movie/movie-browser/src/components/common/ErrorScreen.js
@@ -0,0 +1,31 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import PropTypes from 'prop-types';
+
+
+export default function ErrorScreen(props) {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: props.route.params.errorImgAsset }} style={styles.image} />
+            <Text>{props.message}</Text>
+        </View>
+    )
+}
+
+
+ErrorScreen.propTypes = {
+    loadingImgAsset: PropTypes.string,
+    message: PropTypes.string,
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    image: {
+        width: 300,
+        height: 300,
+    },
+});
